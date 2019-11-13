452f77f9802e362cf30955dc0ae8891281daf41a
Chris West <christopherwest1@mail.weber.edu>
2019-11-12 22:31:48 -0700

    Got Teleportation to work

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject Portal;
+    public GameObject Player;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+    private IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(1);
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+    }
+}
