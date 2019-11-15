68726e5bdaceee06fcc274879831896cbf9bb3d1
Chris West <christopherwest1@mail.weber.edu>
2019-11-14 20:55:40 -0700

    Can transfer to level 2 now - can't walk/jump very well so far

--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleportation : MonoBehaviour
 {
@@ -9,15 +10,27 @@ public class Teleportation : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.tag == "Player" && Portal.gameObject.tag == "Level1_Door")
+        {
+            StartCoroutine(Teleport_Level());
+        }
+
         if (other.gameObject.tag == "Player")
         {
             StartCoroutine(Teleport());
         }
+
     }
 
     private IEnumerator Teleport()
     {
         yield return new WaitForSeconds(1);
         Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+    }
+
+    private IEnumerator Teleport_Level()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
