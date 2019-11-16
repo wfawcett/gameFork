49da7c267c1eb9c7bcc68766cf648bb4253f0ba5
Will Fawcett <wfawcett@gmail.com>
2019-11-16 12:15:33 -0700

    Added bad guy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour{
+    public LayerMask enemyMask;
+    public LayerMask playerMask;
+    public float speed = 1;
+    public float lookAhead = 0.5f;
+    public bool avoidFalling = true;
+    Rigidbody2D myBody;
+    Transform myTransform;
+    float myWidth, myHeight; 
+    bool wasFalling = true; // assume I am falling as I am put on the board above the ground most of the time. 
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()    {
+        anim = this.GetComponentInChildren<Animator>();
+        myTransform = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponentInChildren<SpriteRenderer>().bounds.extents.x;
+        myHeight = this.GetComponentInChildren<SpriteRenderer>().bounds.extents.y;        
+    }
+
+    private void FixedUpdate(){        
+        // set a position relative to me where my line casts should start
+        Vector2 myVec2Position = myTransform.position.toVector2();
+        Vector2 lineCastPos = myVec2Position - myTransform.right.toVector2() * myWidth + Vector2.up * myHeight;
+        
+        // determine if something is in front of me
+        Debug.DrawLine(lineCastPos, lineCastPos - myTransform.right.toVector2() * lookAhead);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - myTransform.right.toVector2() * lookAhead, enemyMask);
+        
+        // determine if I am about to fall off an edge. 
+        bool GroundInFrontOfMe = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down *2, enemyMask);   
+        
+        
+        
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+        
+        
+        
+        
+                
+        // determine if I am touching the player
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(myVec2Position, 0.75f, playerMask);
+        bool touchingPlayer = colliders.Length > 0;
+
+        // If touching the player, end the game (we can update this later with decrement health if we want.)
+        if (touchingPlayer) {            
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+        // if he is blocked he will aways turn, he will turn on an edge if avoidFalling is true
+        if ((!GroundInFrontOfMe && avoidFalling ) || isBlocked)
+        {
+            Vector3 currentRotation = myTransform.eulerAngles;
+            currentRotation.y += 180;
+            myTransform.eulerAngles = currentRotation;            
+        }     
+        
+        Vector2 myVel = myBody.velocity;
+        myVel.x = -myTransform.right.x * speed;
+        myBody.velocity = myVel;
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        
+    }
+}
