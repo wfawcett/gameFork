247f39d6df1d4f6ae81b231a023bed639379e886
Gracie Davenport <glhdavenport@gmail.com>
2019-12-03 22:01:57 -0700

    added green lights everywhere, added green ambient lighting and a sprite difuse material on some things

---
+++ Assets/Scripts/LightPulse.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LightPulse : MonoBehaviour
+{
+    public float duration = 3f;
+    float originalRange;
+
+
+    void Start()
+    {
+       originalRange = GetComponent<Light>().range;
+    }
+
+
+    public void Update()
+    {
+        var amplitude = GetComponent<Light>().range = Mathf.PingPong(Time.time, duration);
+        amplitude = amplitude / duration * 0.5f + 0.5f;
+        GetComponent<Light>().range = originalRange * amplitude;
+
+
+    }
+}
+
+
