60aba98faf54444e5a2d3d216b0bddbd3a6156aa
alovell6 <aaron_lovell@hotmail.com>
2020-01-20 17:20:14 -0700

    half done

---
+++ Assets/DoorOpenScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenScript : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DoorOpens()
+    {
+        anim.SetBool("Opens", true);
+    }
+    public void DoorClose()
+    {
+        anim.SetBool("Opens", false);
+    }
+}
